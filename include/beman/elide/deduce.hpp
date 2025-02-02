38ddd6e77f07a44f95fd7e97ee30856ea4390cce
River <26424577+wusatosi@users.noreply.github.com>
2025-02-01 19:57:38 -0500

    format via pre-commit

--- include/beman/elide/deduce.hpp
+++ include/beman/elide/deduce.hpp
@@ -10,23 +10,20 @@ namespace beman::elide {
 
 namespace detail::deduce {
 
-template<typename>
+template <typename>
 struct is_elide : std::false_type {};
-template<typename T, typename... Args>
+template <typename T, typename... Args>
 struct is_elide<beman::elide::elide<T, Args...>> : std::true_type {};
 
-}
+} // namespace detail::deduce
 
-template<typename T>
+template <typename T>
 struct deduce : std::remove_cvref<T> {};
-template<typename T>
-  requires
-    detail::deduce::is_elide<
-      std::remove_cvref_t<T>>::value &&
-    std::is_invocable_v<T>
+template <typename T>
+    requires detail::deduce::is_elide<std::remove_cvref_t<T>>::value && std::is_invocable_v<T>
 struct deduce<T> : std::invoke_result<T> {};
 
-template<typename T>
+template <typename T>
 using deduce_t = typename deduce<T>::type;
 
 } // namespace beman::elide
